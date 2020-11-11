a0b16a4b785a79754f1fcef30d1b8f6838712ef4
zalwicker2 <zalwicker@gmail.com>
2020-11-10 19:17:10 -0600

    added more stuff

---
+++ ConsoleTechTest/src/Room.java
@@ -0,0 +1,6 @@
+public class Room {
+	int[][] room = new int[8][8];
+	public Room (int[][] room) {
+		this.room = room;
+	}
+}
---
+++ ConsoleTechTest/src/Vector3.java
@@ -0,0 +1,20 @@
+public class Vector3 {
+	public int x, y, z;
+	
+	public Vector3(int x, int y) {
+		this(x, y, 0);
+	}
+	
+	public Vector3(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public Vector3 add(Vector3 v2) {
+		x = x + v2.x;
+		y = y + v2.y;
+		z = z + v2.z;
+		return this;
+	}
+}
